40f4f13fd3b7ded48e9d06a28bd84f6ff9912577
joaquindevoto <jdevoto08@gmail.com>
2026-05-05 11:43:50 -0300

    Update exercise_02_count_words.py

--- exercise_02_count_words.py
+++ exercise_02_count_words.py
@@ -28,4 +28,28 @@ def count_words(filename):
         # archivo contiene: "Hola mundo hola\nmundo python\n"
         count_words("texto.txt") -> {"hola": 2, "mundo": 2, "python": 1}
     """
-    pass  # Reemplazar con tu implementación
+    try:
+        with open(filename, 'r') as archivo:
+            contenido = archivo.read()
+    except FileNotFoundError :
+        raise FileNotFoundError
+
+    dicc= contenido.split()
+    res= {}
+
+    for i in range(len(dicc)):
+        clave = dicc[i]. lower()
+        if clave in res:
+            res[clave]+=1
+        else:
+            res[clave]= 1
+    return res
+
+
+
+
+
+
+
+
+
